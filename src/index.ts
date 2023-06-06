3423d4ec25532fa47231c5b0ae546c158df68e88
Sam Thorne <sam.keim@atomicobject.com>
2023-06-06 08:19:20 -0400

    Testing hidden password implementation

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,21 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import { config } from '../config';
 
-const app = express();
-app.use(cors ({
-    credentials: true
-}));
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
+// const app = express();
+// app.use(cors ({
+//     credentials: true
+// }));
+// app.use(compression());
+// app.use(cookieParser());
+// app.use(bodyParser.json());
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log('server running on http://localhost:8080/');
-});
+// // server.listen(8080, () => {
+//     console.log('server running on http://localhost:8080/');
+// });
+
+const MONGO_URL = 'mongodb+srv://samthorne:' + config.MONGO_DB_PASS + '@cluster0.jvhrs6g.mongodb.net/?retryWrites=true&w=majority';
+console.log(MONGO_URL);
