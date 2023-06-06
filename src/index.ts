68a86af87fe3959250712a4e30d9e9f15dbdb56d
Sam Thorne <sam.keim@atomicobject.com>
2023-06-06 09:35:52 -0400

    Progress with REST API tutorial example

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,24 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import { config } from '../config';
+import mongoose from 'mongoose';
 
-// const app = express();
-// app.use(cors ({
-//     credentials: true
-// }));
-// app.use(compression());
-// app.use(cookieParser());
-// app.use(bodyParser.json());
+const app = express();
+app.use(cors ({
+    credentials: true
+}));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-// // server.listen(8080, () => {
-//     console.log('server running on http://localhost:8080/');
-// });
+server.listen(8080, () => {
+    console.log('server running on http://localhost:8080/');
+});
 
 const MONGO_URL = 'mongodb+srv://samthorne:' + config.MONGO_DB_PASS + '@cluster0.jvhrs6g.mongodb.net/?retryWrites=true&w=majority';
-console.log(MONGO_URL);
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
